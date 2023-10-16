3883596273b925f794f10f515e54175fa6317935
Claire McCarthy <clairemccarthy@google.com>
2023-10-16 13:35:48 -0700

    when a tracker is removed, hold onto its processing metrics until the work item is completed

--- runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/DataflowExecutionStateSampler.java
+++ runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/DataflowExecutionStateSampler.java
@@ -1,7 +1,12 @@
 package org.apache.beam.runners.dataflow.worker;
 
+import java.util.HashMap;
+import java.util.IntSummaryStatistics;
+import java.util.Map;
+import java.util.Map.Entry;
 import org.apache.beam.runners.core.metrics.ExecutionStateSampler;
 import org.apache.beam.runners.core.metrics.ExecutionStateTracker;
+import org.apache.beam.runners.dataflow.worker.DataflowExecutionContext.DataflowExecutionStateTracker;
 import org.joda.time.DateTimeUtils.MillisProvider;
 
 public class DataflowExecutionStateSampler extends ExecutionStateSampler {
@@ -9,6 +14,8 @@ public class DataflowExecutionStateSampler extends ExecutionStateSampler {
   private static final MillisProvider SYSTEM_MILLIS_PROVIDER = System::currentTimeMillis;
   private static final DataflowExecutionStateSampler INSTANCE =
       new DataflowExecutionStateSampler(SYSTEM_MILLIS_PROVIDER);
+
+  private Map<String, Map<String, IntSummaryStatistics>> completedProcessingMetrics = new HashMap<>();
 
   public static DataflowExecutionStateSampler instance() {
     return INSTANCE;
@@ -18,8 +25,30 @@ public class DataflowExecutionStateSampler extends ExecutionStateSampler {
     super(clock);
   }
 
+  private Map<String, IntSummaryStatistics> mergeStepStatsMaps(
+      Map<String, IntSummaryStatistics> map1, Map<String, IntSummaryStatistics> map2) {
+    for (Entry<String, IntSummaryStatistics> steps : map2
+        .entrySet()) {
+      map1.compute(steps.getKey(), (k, v) -> {
+        if (v == null) {
+          return steps.getValue();
+        }
+        v.combine(steps.getValue());
+        return v;
+      });
+    }
+    return map1;
+  }
+
   @Override
-  public void removeTracker(ExecutionStateTracker tracker) {
+  public synchronized void removeTracker(ExecutionStateTracker tracker) {
+    if (tracker instanceof DataflowExecutionContext.DataflowExecutionStateTracker) {
+      DataflowExecutionStateTracker dfTracker = (DataflowExecutionStateTracker) tracker;
+      completedProcessingMetrics.put(dfTracker.getWorkItemId(),
+          mergeStepStatsMaps(completedProcessingMetrics.getOrDefault(
+                  dfTracker.getWorkItemId(), new HashMap<>()),
+              dfTracker.getProcessingTimesByStep()));
+    }
     super.removeTracker(tracker);
   }
 
@@ -28,4 +57,7 @@ public class DataflowExecutionStateSampler extends ExecutionStateSampler {
     super.doSampling(millisSinceLastSample);
   }
 
+  public synchronized void clearMapsForWorkId(String workId) {
+    completedProcessingMetrics.remove(workId);
+  }
 }
